ecf6936f556c76826939824994104b643bd07a27
Alfaraby Indra <alfarabyindra@gmail.com>
2024-02-06 18:38:25 +0700

    tes upload project react ke github

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import React, { useState } from "react";
+import "./App.css";
+import Button from "./components/Button";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
+  const [getValue, setValue] = useState("");
+
+  const changeButton = () => {
+    setValue("My Button");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Navbar />
+        <p>Halo guys saya lagi belajar react js</p>
+        <Button />
+        <Footer />
+        <p>{getValue}</p>
+        <button onClick={() => changeButton()}>Click Here!</button>
       </header>
     </div>
   );
